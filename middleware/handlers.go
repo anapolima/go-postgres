0a58013b216820e58acea45692deede4c06ed05b
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-11-29 14:03:58 -0300

    creating function to create a new user

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,14 +1,16 @@
 package middleware
 
 import (
-	"database/sql" // "encoding/json" // package to encode and decode the json into struct and vice versa
-	"fmt"          // "github.com/gorilla/mux" // used to get the params from the route
-	"log"          // used to access the request and response object of the api
-	"os"           // used to read the environment variable
+	"database/sql"
+	"encoding/json"      // "encoding/json" // package to encode and decode the json into struct and vice versa
+	"fmt"                // "github.com/gorilla/mux" // used to get the params from the route
+	"go-postgres/models" // models package where User schema is defined
+	"log"
+	"net/http" // used to access the request and response object of the api
+	"os"       // used to read the environment variable
 
 	"github.com/joho/godotenv" // package used to read the .env file
 	_ "github.com/lib/pq"      // postgres golang driver
-	// models package where User schema is defined
 	// "strconv" // package used to covert string into int type
 )
 
@@ -46,4 +48,35 @@ func createConnection() *sql.DB {
 
 	// return the connection
 	return db
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	// set the header to content type x-www-form-urlencoded
+	// Allow all origin to handle cors issue
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// create an empty user of type models.User
+	var user models.User
+
+	// decode the json request to user
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body %v", err)
+	}
+
+	// call insert user function and pass the user
+	insertID := insertUser(user)
+
+	// format a response object
+	res := response{
+		ID:      insertID,
+		Message: "User created successfully",
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
 }
