6f4394fffb03b3c5dc27180225641daf44251af3
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-11-29 14:10:41 -0300

    creating function to delete user from the db

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -108,7 +108,6 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-
 // GetAllUsers will return all the users
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
@@ -156,6 +155,39 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
 	// format the message string
 	msg := fmt.Sprintf("User updated successfully. Total rows/record affected %v", updatedRows)
+
+	// format the response message
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
+
+// DeleteUser delete user from the database
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// get the userid from the request params, key is "id"
+	params := mux.Vars(r)
+
+	// convert the id type from string to int
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert string into int. %v", err)
+	}
+
+	// call the deleteUser, convert the id into int64()
+	deletedRows := deleteUser(int64(id))
+
+	// format the message string
+	msg := fmt.Sprintf("User deleted successfully. Total rows/record affected %v", deletedRows)
 
 	// format the response message
 	res := response{
